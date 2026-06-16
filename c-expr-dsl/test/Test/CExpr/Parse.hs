66d7e2f6cafc5799b510bc65d8405f1eaea4a2c0
Dominik Schrempf <dominik@well-typed.com>
2026-06-16 10:01:20 +0200

    Init c-expr-dsl and c-expr-runtime

---
+++ c-expr-dsl/test/Test/CExpr/Parse.hs
@@ -0,0 +1,18 @@
+module Test.CExpr.Parse (
+    tests
+  ) where
+
+import Test.Tasty
+
+import Test.CExpr.Parse.Golden qualified as Golden
+import Test.CExpr.Parse.Literal qualified as Literal
+import Test.CExpr.Parse.Macro qualified as Macro
+import Test.CExpr.Parse.Type qualified as Type
+
+tests :: TestTree
+tests = testGroup "parse" [
+      Type.tests
+    , Macro.tests
+    , Literal.tests
+    , Golden.tests
+    ]
